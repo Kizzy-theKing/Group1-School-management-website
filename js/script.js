b2956ec5807146cb3e8adb62559c5b0e0f04a676
Sekooni Ayodeji King <Sekooniking@gmail.com>
2025-07-07 11:55:47 +0100

    edited teacher

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// courses and review js back to top button
+
+// making the button not to appear at 1st, then appear when scrolling up
+let backToTop = document.getElementById("backToTop");
+backToTop.style.display = "none";
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 500) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+// smooth animation display back to the top
+
+backToTop.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
